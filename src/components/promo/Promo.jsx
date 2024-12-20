8c17cab98742c1c981e668763409eec60aab8522
Andrew <lex.red.15@mail.ru>
2024-12-20 10:50:30 +0300

    фикс ошибок (5)

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -4,19 +4,19 @@ import promoImg from './../../fashion_images/promoImg.png';
 
 const Promo = () => {
     return ( 
-    <section className="promo">
-        <div className="container_promo">
-            <div className="promo_content">
-                <div className="promo_text">
-                    <div className="promo_title">
+    <section className='promo'>
+        <div className='container_promo'>
+            <div className='promo_content'>
+                <div className='promo_text'>
+                    <div className='promo_title'>
                     Elegance in simplicity,<br />
                     Earth’s harmony
                     </div>
-                    <div className="promo_btn-wrapper">
-                      <button href="#!" className="promo_btn">New In</button>
+                    <div className='promo_btn-wrapper'>
+                      <button href="#!" className='promo_btn'>New In</button>
                     </div>
                 </div>
-                <div className="promo_img"><img src={promoImg} alt="hero" /></div>
+                <div className='promo_img'><img src={promoImg} alt="hero" /></div>
             </div>
         </div>
     </section>
